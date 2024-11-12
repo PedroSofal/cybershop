b084b29cc3a4b1ac3298e60dfdf362cc13b782d5
Pedro Sofal <pedrosofal@gmail.com>
2024-11-12 12:27:51 -0300

    corrections for safari

--- client/src/hooks/useHideOnScroll.jsx
+++ client/src/hooks/useHideOnScroll.jsx
@@ -1,17 +1,33 @@
 import { useEffect, useState } from "react";
 
 function useHideOnScroll() {
-  const [ prevScrollpos, setPrevScrollpos ] = useState(window.scrollY);
-  const [ isHidden, setIsHidden ] = useState(false);
+  const [prevScrollpos, setPrevScrollpos] = useState(window.scrollY);
+  const [isHidden, setIsHidden] = useState(false);
+  const [isBouncing, setIsBouncing] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollPos = window.scrollY;
+
+      // Detectar um movimento de "bounce" (pequena rotação que ocorre no topo ou fundo)
+      if (Math.abs(prevScrollpos - currentScrollPos) < 50) {
+        if (!isBouncing) {
+          setIsBouncing(true);
+        }
+        return; // Ignora a rolagem se for um "bounce"
+      } else {
+        if (isBouncing) {
+          setIsBouncing(false);
+        }
+      }
+
+      // Mostrar ou esconder o elemento com base na rolagem
       if (prevScrollpos > currentScrollPos) {
         setIsHidden(false);
       } else {
         setIsHidden(true);
       }
+
       setPrevScrollpos(currentScrollPos);
     };
 
@@ -20,7 +36,7 @@ function useHideOnScroll() {
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, [prevScrollpos]);
+  }, [prevScrollpos, isBouncing]);
 
   return isHidden;
 }
