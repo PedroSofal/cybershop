7bc4c0971a43f19ad2aa0c3f82c5e1df2c717c73
Pedro Sofal <pedrosofal@gmail.com>
2024-11-13 11:49:56 -0300

    new render config

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const baseURL = process.env.NODE_ENV === 'production'
+  ? 'https://cybershop-client.onrender.com'
+  : 'http://192.168.1.8:3000';   
+
 export default axios.create({
-  baseURL: import.meta.env.VITE_AXIOS_BASE_URL,
+  baseURL: baseURL,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
