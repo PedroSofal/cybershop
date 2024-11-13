b46bdef2da2e8a5f693bfb5c5048f65c84679bf9
Pedro Sofal <pedrosofal@gmail.com>
2024-11-13 11:53:04 -0300

    fixed server url

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL = process.env.NODE_ENV === 'production'
-  ? 'https://cybershop-client.onrender.com'
+  ? 'https://cybershop-server.onrender.com'
   : 'http://192.168.1.8:3000';   
 
 export default axios.create({
